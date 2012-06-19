0e7caf5acfd91fc0e6ed5b18a82b289456fb6950
Philippe Elsass <philippe.elsass@gmail.com>
2012-06-19 15:57:54 +0200

    clip looping

--- haxelib/aze/display/TileClip.hx
+++ haxelib/aze/display/TileClip.hx
@@ -48,6 +48,7 @@ class TileClip extends TileSprite
 				animated = false;
 				currentFrame = totalFrames - 1;
 			}
+			setIndice(frames[newFrame]);
 			if (onComplete != null) onComplete(this);
 		}
 		else setIndice(frames[newFrame]);
